c4b378b80356ce25654ad64f4375173b844f9adf
leshec <none>
2024-06-11 00:22:18 +0100

    do hash

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 fn main() {
-    let set = String::from("abc");
+    //convert file to haskset
+
+    let set = String::from("abcdefg");
+    let set_size: usize = set.len();
     let sets: Vec<char> = set.chars().collect();
 
+    //is there a way just to directly work
+    //with binary or bools without using strings?
     let mut binary_list: Vec<String> = Vec::new();
-    for n in 0..8 {
-        binary_list.push(format!("{n:03b}"));
+    for n in 0..2_u32.pow(set_size.try_into().unwrap()) {
+        //the value 7 here for 7 letters
+        //how to insert a set_size into the variable?
+        binary_list.push(format!("{n:07b}"));
     }
 
     let mut binary_vec: Vec<Vec<char>> = Vec::new();
@@ -13,9 +20,9 @@ fn main() {
         binary_vec.push(word.chars().collect());
     }
 
-    println!("{:?}", &binary_vec);
-    assert_eq!(sets, ['a', 'b', 'c']);
-    println!("{:?}", &set);
+    //println!("{:?}", &binary_vec);
+    //assert_eq!(sets, ['a', 'b', 'c']);
+    //println!("{:?}", &set);
 
     let mut subsets: Vec<String> = Vec::new();
 
@@ -31,6 +38,7 @@ fn main() {
         }
         buff.clear();
     }
+    subsets.sort();
 
     println!("{:?}", subsets);
 }
