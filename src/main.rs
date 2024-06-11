8e3ccfdb23ba461f511a286acbfcd6746a67fe29
Richard <richard.mcewan100@gmail.com>
2024-06-11 15:33:50 +0100

    need to fix bug

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 //get rid of comments
+//bug: only finding one occurence of combination, probably
+//due to the hashmap
 
 fn main() {
     fn read_file(filename: &str) -> Result<Vec<String>, std::io::Error> {
@@ -19,7 +21,7 @@ fn main() {
     let mut dictionary: HashMap<String, String> = HashMap::new();
 
     for line in lines.iter() {
-        let mut word: Vec<char> = line.chars().collect();
+        let mut word: Vec<char> = line.trim_end().chars().collect();
         word.sort();
         let alpabeticised_word: String = word.into_iter().collect();
         dictionary.insert(line.to_string(), alpabeticised_word);
@@ -28,14 +30,13 @@ fn main() {
     //println!("{:?}", dictionary);
     //autogenerate this later...
     //add scores etc and weighted distribution
-    let set = String::from("bastard");
-    let set_size: usize = set.len();
+    let set = String::from("decimal");
     let sets: Vec<char> = set.chars().collect();
 
     //is there a way just to directly work
     //with binary or bools without using strings?
     let mut binary_bytes: Vec<String> = Vec::new();
-    for n in 0..2_u32.pow(set_size.try_into().unwrap()) {
+    for n in 0..2_u32.pow(7) {
         //the value 7 here for 7 letters
         //how to insert a set_size variable into the formatter
         //else can I just compose a string to replace the {}
@@ -68,14 +69,24 @@ fn main() {
     }
     subsets.sort();
 
+    let mut results_list: Vec<String> = Vec::new();
+
     //println!("{:?}", subsets);
     // Look up the values associated with some keys.
+    //probably need tomake this a function and call more than once
+    //as there is a bug missing anagrams
+    //the algo will find decimal but not claimed for example
     for key in subsets.iter() {
         match dictionary.get(key) {
-            Some(value) => println!("{key}: {value} "),
+            Some(value) => {
+                println!("{key}: {value}");
+                results_list.push(key.clone());
+            }
             None => continue,
         }
     }
+
+    println!("Results list {:?}", results_list);
 
     //Ok so I can get the word i want from dictinary the key above.
     //stick keys in a list, score them and take the highest score
@@ -91,6 +102,32 @@ fn main() {
     // - 5 points: K
     // - 8 points: J, X
     // - 10 points: Q, Z
+
+    fn score_word(word: String) -> usize {
+        //this repeats an earlier process
+        let mut letters: Vec<char> = Vec::new();
+        let mut score: usize = 0;
+
+        for letter in word.chars() {
+            letters.push(letter);
+        }
+        
+        for letter in letters.iter(){
+            match letter {
+                
+                E, A, I, O, N, R, T, L, S, U => score +=1,
+        D, G => score +=2,
+        B, C, M, P =>score +=3,
+        F, H, V, W, Y => score +=4,
+        K => score +=5,
+        J, X => score +=8,
+        Q, Z =>score +=10,
+            }
+        }
+        //return letters
+        return 1;
+    }
+
     //
     //     - 12 tiles: E
     // - 9 tiles: A,I
