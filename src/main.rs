d72018738081307c529f87aee6c1928e0c045ec2
Richard <richard.mcewan100@gmail.com>
2024-06-19 17:01:04 +0100

    basic is down tidy up now

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,20 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-//bug: only finding one occurence of combination e.g. decimal and claimed.
-//i want both.
+//Tidy up a lot.
+//put functions, variables etc in sensible locations
+//some variables should be structs e.g. result word and score are coupled
+//use appropriate functions
+//simply the subsets building, too mnay allocations/types
+//best way to read file and ingest/process?
+//add tests
 
-//Create random bag seven tiles
+fn read_file(filename: &str) -> Result<Vec<String>, std::io::Error> {
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+    reader.lines().collect()
+}
+
 fn random_ascii() -> String {
     let mut bag = String::new();
     const ASCIISET: &[u8] =
@@ -21,15 +31,41 @@ fn random_ascii() -> String {
     return bag;
 }
 
+fn score_word(word: String) -> usize {
+    //this repeats an earlier process
+    let mut letters: Vec<char> = Vec::new();
+    let mut score: usize = 0;
+
+    for letter in word.chars() {
+        letters.push(letter);
+    }
+
+    for letter in letters.iter() {
+        match letter {
+            'e' | 'a' | 'i' | 'o' | 'n' | 'r' | 't' | 'l' | 's' | 'u' => score += 1,
+            'd' | 'g' => score += 2,
+            'b' | 'c' | 'm' | 'p' => score += 3,
+            'f' | 'h' | 'v' | 'w' | 'y' => score += 4,
+            'k' => score += 5,
+            'j' | 'x' => score += 8,
+            'q' | 'z' => score += 10,
+            _ => println!("missed a case"),
+        }
+    }
+    return score;
+}
+
 fn main() {
     let set = random_ascii();
+    //let set = "decimal"; //for testing purposes
     println!("The starting bag is {}", set);
     let sets: Vec<char> = set.chars().collect();
 
     //how can i work more directly with binary?
+    //can i just have arrary of bools or read u8 as bytes/bits
     let mut binary_bytes: Vec<String> = Vec::new();
     for n in 0..2_u32.pow(7) {
-        //could generalise the input 7
+        //could generalise the input 7 etc
         binary_bytes.push(format!("{n:07b}"));
     }
 
@@ -53,7 +89,6 @@ fn main() {
             }
         }
         if !buff.is_empty() {
-            //&& !subsets.contains(&buff) {
             buff.sort_by(|a, b| a.cmp(b));
             let buff: &String = &buff.clone().into_iter().collect();
             subsets.push(buff.to_string());
@@ -61,21 +96,10 @@ fn main() {
         buff.clear();
     }
 
-    // subsets.sort_by(|a, b| a.cmp(b));
-    // println!("*****************");
-    // println!("{:?}", &subsets);
-
-    fn read_file(filename: &str) -> Result<Vec<String>, std::io::Error> {
-        let file = File::open(filename)?;
-        let reader = BufReader::new(file);
-        reader.lines().collect()
-    }
-
     let filename = "data";
     let lines = read_file(filename).unwrap();
     //    println!("{:?}", lines);
 
-    //https://doc.rust-lang.org/std/collections/struct.HashMap.html
     let mut dictionary: HashMap<String, String> = HashMap::new();
 
     for line in lines.iter() {
@@ -85,6 +109,7 @@ fn main() {
         dictionary.insert(line.trim().to_string(), alpabeticised_word);
     }
 
+    //Finding words that match subsets of tiles
     let mut results_list: Vec<String> = Vec::new();
     for (key, value) in dictionary {
         if subsets.contains(&value) {
@@ -92,31 +117,19 @@ fn main() {
         }
     }
 
+    let mut answer: Vec<(usize, String)> = Vec::new();
     for result in results_list.iter() {
-        println!("The word {}:{}", result, score_word(result.to_string()));
+        answer.push((score_word(result.clone()), result.clone()));
     }
-
-    fn score_word(word: String) -> usize {
-        //this repeats an earlier process
-        let mut letters: Vec<char> = Vec::new();
-        let mut score: usize = 0;
-
-        for letter in word.chars() {
-            letters.push(letter);
-        }
-
-        for letter in letters.iter() {
-            match letter {
-                'e' | 'a' | 'i' | 'o' | 'n' | 'r' | 't' | 'l' | 's' | 'u' => score += 1,
-                'd' | 'g' => score += 2,
-                'b' | 'c' | 'm' | 'p' => score += 3,
-                'f' | 'h' | 'v' | 'w' | 'y' => score += 4,
-                'k' => score += 5,
-                'j' | 'x' => score += 8,
-                'q' | 'z' => score += 10,
-                _ => println!("missed a case"),
-            }
-        }
-        return score;
-    }
+    answer.sort();
+    // println!("The answer is {:#?}", answer);
+    println!("The list of words are {:?}", &results_list);
+    println!(
+        "Among the top five scoring words are {:?}{:?}{:?}{:?}{:?}",
+        answer.pop().unwrap(),
+        answer.pop().unwrap(),
+        answer.pop().unwrap(),
+        answer.pop().unwrap(),
+        answer.pop().unwrap()
+    );
 }
